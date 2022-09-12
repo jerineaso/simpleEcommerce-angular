39897c0fbb21c18b34a285a3081d0f98cff59ad2
jerineaso <jerin.easo.20@gmail.com>
2022-09-12 11:00:35 +0530

    formarray

--- src/app/admin/manipulation/manipulation.component.ts
+++ src/app/admin/manipulation/manipulation.component.ts
@@ -9,7 +9,7 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 export class ManipulationComponent implements OnInit {
 
   formContainer !: FormGroup
-  addDataForm !: FormArray;
+  // addDataForm !: FormArray;
 
   constructor(private fb : FormBuilder) {
   
@@ -17,34 +17,49 @@ export class ManipulationComponent implements OnInit {
 
   ngOnInit(): void {
     this.formContainer = this.fb.group({
-      addDataForm : this.fb.array([this.fb.group({
-        itemName: new FormControl('',[
-          Validators.required,
-          Validators.maxLength(100),
-          Validators.pattern("[a-zA-Z][a-zA-Z ]+")
-        ]),
-        itemCode : new FormControl('',[
-          Validators.required,
-          Validators.pattern("^[a-zA-Z0-9]{5}$")
-        ]),
-        itemDescrip : new FormControl('',[
-          Validators.required,
-          Validators.maxLength(500)
-        ]),
-        itemPrice : new FormControl('',[
-          Validators.required,
-        ]),
-        itemImage : ['']
-      })])
+      addDataForm : this.fb.array(
+        [this.createItemData()]
+      )
     })
   }
 
-  get admins() {
-    return this.formContainer.controls["addDataForm"] as FormArray;
+  // get admins() {
+  //   return this.formContainer.controls["addDataForm"] as FormArray;
+  // }
+
+  get itemData(): FormArray{
+    return this.formContainer.get('formContainer.addFormData') as FormArray;
   }
 
-  onSubmit(){
+  createItemData(){
+    return this.fb.group({
+      itemName: new FormControl('',[
+        Validators.required,
+        Validators.maxLength(100),
+        Validators.pattern("[a-zA-Z][a-zA-Z ]+")
+      ]),
+      itemCode : new FormControl('',[
+        Validators.required,
+        Validators.pattern("^[a-zA-Z0-9]{5}$")
+      ]),
+      itemDescrip : new FormControl('',[
+        Validators.required,
+        Validators.maxLength(500)
+      ]),
+      itemPrice : new FormControl('',[
+        Validators.required,
+      ]),
+      itemImage : ['']
+    })
+  }
 
+  onSubmit(){ 
+    // console.log(this.createItemData);
+    
+    // let data = this.createItemData()
+    // this.itemData.push(data)
+    // console.log(this.itemData);
     console.log(this.formContainer.value);
+    
   }
 }
